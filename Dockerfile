82073accb584ea9a8e6c39687f4e089b4cba82ec
red_hoodie <rudrakshigupta305@gmail.com>
2023-02-26 20:52:03 +0530

    DB updated and working

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM mysql:latest
 ENV MYSQL_ROOT_PASSWORD=root
+RUN mysql_upgrade -p
 COPY ./db-dump/database_insc.sql /docker-entrypoint-initdb.d/
--- db-dump/database_insc.sql
+++ db-dump/database_insc.sql
@@ -3,14 +3,14 @@ USE ebook_mems;
 
 CREATE TABLE members(
     Userid int not null,
-    Uname varchar not null,
-    Email varchar not null unique,
-    Phoneno varchar not null unique,
-    PasswordU varchar not null,
-    addrss varchar,
-    Landmark varchar,
-    City varchar,
-    StateU varchar,
+    Uname varchar(50) not null,
+    Email varchar(20) not null unique,
+    Phoneno int not null unique,
+    PasswordU varchar(10) not null,
+    addrss varchar(100),
+    Landmark varchar(100),
+    City varchar(30),
+    StateU varchar(30),
     zip int not null,
     PRIMARY KEY(Userid)
 );

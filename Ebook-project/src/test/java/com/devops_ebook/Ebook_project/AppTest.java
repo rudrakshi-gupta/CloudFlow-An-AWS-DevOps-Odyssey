5364e9d496b843ad1609db042854a5335b90d1fa
red_hoodie <rudrakshigupta305@gmail.com>
2023-02-28 23:04:49 +0530

    Maven project Ebook

---
+++ Ebook-project/src/test/java/com/devops_ebook/Ebook_project/AppTest.java
@@ -0,0 +1,20 @@
+package com.devops_ebook.Ebook_project;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}

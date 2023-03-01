5ca700e7e047f8c3789d3d36e75d57852e2ba444
red_hoodie <rudrakshigupta305@gmail.com>
2023-03-01 10:54:07 +0530

    Entity user class

---
+++ Ebook-project/src/main/java/com/entity/User.java
@@ -0,0 +1,81 @@
+package com.entity;
+
+public class User {
+	
+	private int id;
+	private String name;
+	private String password;
+	private int phoneno;
+	private String address;
+	private String Landmark;
+	private String city;
+	private String state;
+	private int zip;
+	
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getPhoneno() {
+		return phoneno;
+	}
+	public void setPhoneno(int phoneno) {
+		this.phoneno = phoneno;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getLandmark() {
+		return Landmark;
+	}
+	public void setLandmark(String landmark) {
+		Landmark = landmark;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public int getZip() {
+		return zip;
+	}
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", password=" + password + ", phoneno=" + phoneno + ", address="
+				+ address + ", Landmark=" + Landmark + ", city=" + city + ", state=" + state + ", zip=" + zip + "]";
+	}
+	
+}

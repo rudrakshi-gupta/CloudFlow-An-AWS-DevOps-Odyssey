c2c98e176bc91fda42557f1cd04eae1b4323405f
red_hoodie <rudrakshigupta305@gmail.com>
2023-03-02 18:47:10 +0530

    DAO and its implementation

--- Ebook-project/src/main/java/com/entity/User.java
+++ Ebook-project/src/main/java/com/entity/User.java
@@ -4,6 +4,7 @@ public class User {
 	
 	private int id;
 	private String name;
+	private String email;
 	private String password;
 	private int phoneno;
 	private String address;
@@ -29,6 +30,14 @@ public class User {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	public String getPassword() {
 		return password;
 	}
---
+++ Ebook-project/src/main/java/com/user/servlet/registerSevlet.java
@@ -0,0 +1,7 @@
+package com.user.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+public class registerSevlet extends HttpServlet {
+	
+}

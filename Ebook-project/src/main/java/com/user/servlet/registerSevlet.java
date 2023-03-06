44a04c859b98dd1ecd4e5cc4561ec40097f3c410
red_hoodie <rudrakshigupta305@gmail.com>
2023-03-06 21:24:30 +0530

    Register Servlet

--- Ebook-project/src/main/java/com/user/servlet/registerSevlet.java
+++ Ebook-project/src/main/java/com/user/servlet/registerSevlet.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.DAO.UserDAOImpl;
 import com.DB.DBconnect;
@@ -26,13 +27,34 @@ public class registerSevlet extends HttpServlet {
 			
 //			System.out.println(name+" "+email+" "+phoneno+" "+password+" "+check);
 			
+			HttpSession session = req.getSession();
+			
 			User us = new User();
 			us.setName(name);
 			us.setEmail(email);
 			us.setPhoneno(phoneno);
 			us.setPassword(password);
 			
-			UserDAOImpl dao = new UserDAOImpl(DBconnect.getConn());
+			if(check!=null) {
+			
+				UserDAOImpl dao = new UserDAOImpl(DBconnect.getConn());
+				boolean f = dao.userRegister(us);
+				if(f) {
+//					System.out.println("User Register Success");
+					session.setAttribute("succMsg", "Registeration Success...");
+					resp.sendRedirect("register.jsp");
+				}
+				else {
+//					System.out.println("Something went wrong on the server TRY AGAIN....");
+					session.setAttribute("failedMsg", "Something went wrong on the server TRY AGAIN....");
+					resp.sendRedirect("register.jsp");
+				}
+			}
+			else {
+//				System.out.println("Please CHECK and AGREE terms and conditions!!");
+				session.setAttribute("failedMsg", "Please CHECK and AGREE terms and conditions!!");
+				resp.sendRedirect("register.jsp");
+			}
 		}
 		catch(Exception e) {
 			

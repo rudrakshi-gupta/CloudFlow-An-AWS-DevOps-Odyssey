51b6e2f9efc5f3c510ef4bfa20049708c0b450fa
radhika khanna <khannaradhika08@gmail.com>
2023-03-10 16:52:17 +0530

    ls2

--- Ebook-project/src/main/java/com/user/servlet/LoginServlet.java
+++ Ebook-project/src/main/java/com/user/servlet/LoginServlet.java
@@ -14,12 +14,28 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
         try {
 
+            UserDAOImpl dao=new UserDAOImpl (DBConnect.getConn());
+
+            HttpSession session=req.getSession();
+
             String email=req.getParameter("email");
             String password=req.getParameter("password");
 
             if ("admin@gmail.com".equals(email) && "admin".equals(password)) {
-              resp. sendRedirect("admin/home.jsp");
+                User us new User();
+                session.setAttribute("userobj", us);
+                resp. sendRedirect("admin/home.jsp");
             } else {
+
+                User us=dao.login (email, password);
+                if(us!=null){
+                  session.setAttribute("userobj", us);
+                  resp.send Redirect ("home.jsp");
+                }
+                else {
+                    session.setAttribute("failedMsg", "Email & Password Invalid");
+                    resp.sendRedirect ("login.jsp");
+                }
               resp.sendRedirect ("home.jsp");
             }
         } catch (Exception e) {

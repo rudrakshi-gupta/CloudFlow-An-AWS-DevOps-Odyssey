159b0a1af3a057ef19599f74f559c3895faaee91
radhika khanna <khannaradhika08@gmail.com>
2023-03-10 16:45:07 +0530

    sql

--- Ebook-project/src/main/java/com/DAO/UserDAOImpl.java
+++ Ebook-project/src/main/java/com/DAO/UserDAOImpl.java
@@ -34,5 +34,34 @@ public class UserDAOImpl implements UserDAO {
 		}
 		return f;
 	}
-	
+
+	public User login(String email, String password) {
+       User us = null;
+
+       try {
+           String sql = "select * from user where email= ? and password=?";
+           PreparedStatement ps = conn.prepareStatement(sql);
+           ps.setString(1, email);
+           ps.setString(2, password);
+
+           ResultSet rs = ps.executeQuery();
+           while (rs.next()) {
+               us = new User();
+               us.setId(rs.getInt (1));
+               us.setName (rs.getString(2));
+               us.setEmail (rs.getString(3));
+               us.setPhno (rs.getString(4));
+               us.setPassword (rs.getString(5));
+               us.setAddress (rs.getString(6));
+               us.setLandmark (rs.getString(7));
+               us.setCity (rs.getString(8));
+               us.setState(rs.getString(9));
+               us.setPincode(rs.getString(10));
+            }
+	    } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return us;
+    }
 }

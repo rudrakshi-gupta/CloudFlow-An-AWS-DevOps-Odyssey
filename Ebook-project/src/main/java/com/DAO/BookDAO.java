9a29cff5718b545ddabe8b9c249a0a68d4850511
Harpreet Narwal <62388865+Harpreet-Narwal@users.noreply.github.com>
2023-03-31 12:56:39 +0530

    Add files via upload

--- Ebook-project/src/main/java/com/DAO/BookDAO.java
+++ Ebook-project/src/main/java/com/DAO/BookDAO.java
@@ -2,8 +2,12 @@ package com.DAO;
 
 import java.util.List;
 
+import com.DB.BookDtls;
+
 public interface BookDAO {
 	public boolean addBooks(BookDtls b);
-	public List<BookDtls> getAllBooks();
-	public BookDtls getBookByID(int id);
+	
+	
+//	public List<BookDtls> getAllBooks();
+//	public BookDtls getBookByID(int id);
 }

63fdb48bb77cdcfbb180b087108b85a8ac3c51b4
radhika khanna <khannaradhika08@gmail.com>
2023-04-19 22:41:18 +0530

    2

--- Ebook-project/src/main/java/com/DAO/BookDAO.java
+++ Ebook-project/src/main/java/com/DAO/BookDAO.java
@@ -6,6 +6,5 @@ public interface BookDAO {
 	public boolean addBooks(BookDtls b);
 	
 	
-//	public List<BookDtls> getAllBooks();
-//	public BookDtls getBookByID(int id);
+	public BookDtls getAllBooks();
 }
